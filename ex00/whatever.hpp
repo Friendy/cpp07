ddf5e8f07eac7fb3b8e4a91b37098177ae7493af
mrubina <friendy.m@gmail.com>
2024-04-11 20:40:32 +0200

    ex00 done

---
+++ ex00/whatever.hpp
@@ -0,0 +1,30 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void swap(T &x, T &y)
+{
+	T temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+template <typename T>
+T min(T x, T y)
+{
+	if (y <= x)
+		return(y);
+	else
+		return(x);
+}
+
+template <typename T>
+T max(T x, T y)
+{
+	if (y >= x)
+		return(y);
+	else
+		return(x);
+}
+#endif
