aa1d364b2f865332d35981a25c08f9b619919ff6
mrubina <friendy.m@gmail.com>
2024-04-14 00:03:26 +0200

    ex01 edited, tests added

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,14 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrubina <mrubina@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 23:48:47 by mrubina           #+#    #+#             */
+/*   Updated: 2024/04/13 23:59:18 by mrubina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef WHATEVER_HPP
 # define WHATEVER_HPP
+# include <iostream>
+# include <string>
 
 template <typename T1>
-void print_el(T1 el)
+void print_el(T1 &el)
 {
 	std::cout << el << ", ";
 }
 
 template <typename T>
 void iter(T *arr, size_t len, void f(T&))
+{
+	for (size_t i = 0; i < len; i++)
+	{
+		f(arr[i]);
+	}
+}
+
+template <typename T>
+void iter(T const *arr, size_t len, void f(T const &))
 {
 	for (size_t i = 0; i < len; i++)
 	{
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,10 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrubina <mrubina@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 23:48:43 by mrubina           #+#    #+#             */
+/*   Updated: 2024/04/13 23:58:16 by mrubina          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <iostream>
-#include <string>
 #include "iter.hpp"
 
-
-void word_length(std::string &s)
+void word_length(const std::string &s)
 {
 	std::cout << s.length() << std::endl;
 }
@@ -19,18 +27,59 @@ void replace_arr(std::string &s)
 	s = "haha";
 }
 
-int main(void)
+struct climate
 {
+	std::string room;
+	float temperature;
+};
+
+void init_climate(climate &cl, const std::string room, float temp)
+{
+	cl.room = room;
+	cl.temperature = temp;
+}
+
+void show_climate(climate &cl)
+{
+	std::cout << "the temperature in the " << cl.room << " is " << cl.temperature << std::endl;
+}
+
+void test1()
+{
+	std::cout << "******* string test******"<< std::endl;
 	std::string words[3] = {"test", "electricity", "mouse"};
-	int arr[4] = {3, 7, 45, 75};
 	iter(words, 3, print_el);
 	std::cout << std::endl;
 	iter(words, 3, word_length);
+	iter(words, 3, replace_arr);
+	iter(words, 3, print_el);
+	std::cout << std::endl;
+}
+
+void test2()
+{
+	std::cout << "******* int test******"<< std::endl;
+	int arr[4] = {3, 7, 45, 75};
+	iter(arr, 4, print_el);
+	std::cout << std::endl;
 	iter(arr, 4, replace_num);
 	iter(arr, 4, print_el);
 	std::cout << std::endl;
-	iter(words, 3, replace_arr);
-	iter(words, 3, print_el);
+}
 
+void test3()
+{
+	std::cout << "******* structure test******"<< std::endl;
+	climate cl[2];
+	init_climate(cl[0], "kitchen", 20.5);
+	init_climate(cl[1], "bedroom", 22.2);
+	iter(cl, 2, show_climate);
+}
+
+int main(void)
+{
+	test1();
+	test2();
+	test3();
 	return 0;
 }
